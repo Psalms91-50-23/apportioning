69621c0b2bfd2bccf954759fac71009b6a88bfca
kho thach <khothach@gmail.com>
2023-12-06 19:53:04 +1300

    changed title at wrong place, amended

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,8 @@ import { Apportioning } from '@/components'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Apportioning</title>
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Apportioning/>
-      </main>
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Apportioning/>
+    </main>
   )
 }
