9f38c56455bf5a1469fa70f20ce9991c68e22e31
kho thach <khothach@gmail.com>
2023-12-06 19:49:24 +1300

    added custom title for webpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import { Apportioning } from '@/components'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Apportioning/>
-    </main>
+    <>
+      <Head>
+        <title>Apportioning</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Apportioning/>
+      </main>
+    </>
   )
 }
